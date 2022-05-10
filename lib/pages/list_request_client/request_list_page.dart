8eee1814292c5c6d7b31c98e41ef0125204ff4d5
axelmiranda.845 <axelmiranda.845@gmail.com>
2022-05-10 17:26:43 -0400

    Remover elemento de la lista

--- lib/pages/list_request_client/request_list_page.dart
+++ lib/pages/list_request_client/request_list_page.dart
@@ -3,6 +3,8 @@ import 'package:taxi_segurito_app/models/client_request.dart';
 import 'package:taxi_segurito_app/pages/list_request_client/location.dart';
 import 'package:taxi_segurito_app/pages/list_request_client/request_list_functionality.dart';
 import 'package:taxi_segurito_app/pages/list_request_client/widgets/request_list.dart';
+import 'package:taxi_segurito_app/pages/list_request_client/widgets/request_list_item.dart';
+
 
 class ListRequestClient extends StatefulWidget {
   ListRequestClient({Key? key}) : super(key: key);
@@ -12,7 +14,9 @@ class ListRequestClient extends StatefulWidget {
 }
 
 class _ListRequestClientState extends State<ListRequestClient> {
-  List<ClienRequest> listRequest = [];
+  late List<ClienRequest> listRequest;
+  late GlobalKey<RefreshIndicatorState> refreshListKey;
+  RequestList requestList = new RequestList(); 
 
   ListRequestClientFunctionality listRequestClientFunctionality =
       new ListRequestClientFunctionality();
@@ -24,15 +28,112 @@ class _ListRequestClientState extends State<ListRequestClient> {
         listRequestClientFunctionality.initServiceRequest();
       }
     });
-    LocationService locationService = new LocationService();
-    locationService.initUbicacion().then((value) {
-      locationService.getUbication();
+
+    refreshListKey = new GlobalKey<RefreshIndicatorState>();
+    listRequest = new List<ClienRequest>.empty(growable: true);
+  }
+
+  Widget showList(){
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return new Container(
+      height: height,
+      width: width,
+      child: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: requestList.listRequest!.length,
+        itemBuilder: (BuildContext context, int index){
+          return rowItem(context, index); 
+        }
+      )
+    );
+  }
+
+  Widget rowItem(context, index){
+      dynamic dinamycOb = requestList.listRequest![index];
+      
+      return Dismissible(
+        key: Key(requestList.listRequest![index].toString()),
+        onDismissed: (direction){
+          var item = requestList.listRequest![index];
+          showSnackBar(context, item, index);
+          removeItem(index);   
+        },
+        background: deleteItem(),
+        
+        child: Card(
+          /*child:ListTile(
+            title: Text(requestList.listRequest![index].toString()),
+          ),*/
+          
+          child: new RequestListItem(
+            clientRequest: dinamycOb,
+            callbackRequest: (value) {
+            requestList.callback!(value);},
+          ),
+        ),
+      );
+  }
+
+  showSnackBar(context, item, index){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$item removido de la lista'),
+        action: SnackBarAction(
+          label: "NO REMOVER SOLICITUD",
+          onPressed: (){
+            undoDelete(index, item);
+          }
+        ),
+      )
+    );
+  }
+
+  Future<Null> refreshList() async {
+    await Future.delayed(Duration(seconds: 1));
+    addRandomItem();
+    return null;
+  }
+
+  addRandomItem(){
+    //var nextItem = random.nextInt(200);
+    /*setState(() {
+      listRequest.add(nextItem);
+
+    });*/
+    listRequestClientFunctionality.updateListRequest = ((value) {
+      setState(() {
+        listRequest = value;
+        requestList.listRequest = listRequest;
+      });
     });
+  }
+
+  undoDelete(index, item){
+    setState(() {
+      requestList.listRequest!.insert(index, item);
+    });
+  }
+
+  removeItem(index){
+    setState((){
+      requestList.listRequest!.removeAt(index);
+    });
+  }
+
+  Widget deleteItem(){
+    return Container(
+      alignment: Alignment.centerRight,
+      padding: EdgeInsets.only(right: 20),
+      color: Colors.blue,
+      child: Icon(Icons.delete, color: Colors.white),
+
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    RequestList requestList = new RequestList();
+   
     requestList.listRequest = listRequest;
     requestList.callback = (value) {};
 
@@ -63,6 +164,7 @@ class _ListRequestClientState extends State<ListRequestClient> {
     );
     return Scaffold(
       appBar: appbar,
+
       body: Container(
         color: Color.fromARGB(255, 248, 248, 248),
         child: Column(
@@ -71,8 +173,17 @@ class _ListRequestClientState extends State<ListRequestClient> {
           children: [
             title,
             Expanded(
-              child: Container(child: requestList),
-            )
+              child: Container(
+                child: RefreshIndicator(
+                  key: refreshListKey,
+                  child: showList(),
+                  onRefresh: () async {
+                    await refreshList();
+                  },
+                ),
+              ),
+              
+            ),
           ],
         ),
       ),
