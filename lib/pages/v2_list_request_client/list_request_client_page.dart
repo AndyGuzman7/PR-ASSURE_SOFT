62e7d8a0e83df3d5ff5678dc6fa6d0a18ccd3f5c
axelmiranda.845 <axelmiranda.845@gmail.com>
2022-05-13 10:08:32 -0400

    cambios en request list client

--- lib/pages/v2_list_request_client/list_request_client_page.dart
+++ lib/pages/v2_list_request_client/list_request_client_page.dart
@@ -50,7 +50,7 @@ class _ListRequestClientState extends State<ListRequestClient> {
     dynamic dinamycOb = requestList.listRequest![index];
 
     return Dismissible(
-      key: Key(listRequest[index].idUser.toString()),
+      key: Key(listRequest[index].toString()),
       onDismissed: (direction) {
         var item = listRequest[index];
         showSnackBar(context, item, index);
@@ -59,10 +59,6 @@ class _ListRequestClientState extends State<ListRequestClient> {
       resizeDuration: new Duration(seconds: 2),
       background: deleteItem(),
       child: Card(
-        /*child:ListTile(
-            title: Text(requestList.listRequest![index].toString()),
-          ),*/
-
         child: new RequestListItem(
           clientRequest: dinamycOb,
           callbackRequest: (value) {
@@ -91,11 +87,7 @@ class _ListRequestClientState extends State<ListRequestClient> {
   }
 
   addRandomItem() {
-    //var nextItem = random.nextInt(200);
-    /*setState(() {
-      listRequest.add(nextItem);
-
-    });*/
+    
     listRequestClientFunctionality.updateListRequest = ((value) {
       setState(() {
         listRequest = value;
@@ -107,13 +99,13 @@ class _ListRequestClientState extends State<ListRequestClient> {
 
   undoDelete(index, item) {
     setState(() {
-      requestList.listRequest!.insert(index, item);
+      listRequest.insert(index, item);
     });
   }
 
   removeItem(index) {
     setState(() {
-      requestList.listRequest!.removeAt(index);
+      listRequest.removeAt(index);
     });
   }
 
