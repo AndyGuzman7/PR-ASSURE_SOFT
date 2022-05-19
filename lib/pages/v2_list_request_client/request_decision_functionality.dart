6b9b1dbaad7185b9388c8f185182036bd46020c7
Nikypm <pnicolemilenka@gmail.com>
2022-05-18 21:22:30 -0400

    Modificacion notificacion hacia el taxista

--- lib/pages/v2_list_request_client/request_decision_functionality.dart
+++ lib/pages/v2_list_request_client/request_decision_functionality.dart
@@ -2,11 +2,10 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:taxi_segurito_app/models/taxirequest.dart';
 
 class RequestDecisionFunctionality {
-  List<RequestTaxi> listRequest = [];
-  late final nameBranch = "Taxista";
   late final dbRef;
   late String key;
-  late bool status = false, statusRequest = false, estado = false;
+  late bool statusRequest = false, estado = false;
+  late String idRequestAccept = "";
   late Function(bool) updateStatus;
 
   RequestDecisionFunctionality();
@@ -14,10 +13,10 @@ class RequestDecisionFunctionality {
     dbRef = FirebaseDatabase.instance.reference();
     checkStatus('-N1vHdpBe2km7i6xJbkz');
 
-    Stream<Event> streamBuilder = dbRef.child("RequestPruebas").onValue;
+    Stream<Event> streamBuilder = dbRef.child("RequestTaxi").onValue;
     streamBuilder.listen((event) {
       DataSnapshot snapshot = event.snapshot;
-      getItemsRequestFirebase(snapshot, '-N1vHdpBe2km7i6xJbkz');
+      getItemsRequestFirebase(snapshot, 1);
     });
   }
 
@@ -34,16 +33,22 @@ class RequestDecisionFunctionality {
       print('Esta ocupado conduciendo...');
     } else {
       estado = true;
-      print('No esta ocupado conduciendo...');
+      print('No esta conduciendo...');
     }
   }
 
-  Future<void> update(String idUserTaxista) async {
+//Update data status Taxista and confirmation RequestTaxi
+  Future<void> updateStatusRequest(String idUserTaxista) async {
     dbRef
         .reference()
-        .child(nameBranch)
+        .child("Taxista")
         .child(idUserTaxista)
         .update({'estado': 'ocupado'});
+    dbRef
+        .reference()
+        .child("RequestTaxi")
+        .child(idRequestAccept)
+        .update({'confirmation': true});
   }
 
   Stream<Event> getEvent() {
@@ -54,32 +59,26 @@ class RequestDecisionFunctionality {
     return dbRef;
   }
 
-  getItemsRequestFirebase(DataSnapshot snapshot, String idTaxista) {
-    List<RequestTaxi> listRequestPreview = [];
-
+//Get data from RequestTaxi node
+  getItemsRequestFirebase(DataSnapshot snapshot, int idTaxista) {
     final extractedData = snapshot.value;
-    if (extractedData != null)
+    if (extractedData != null) {
+      statusRequest = false;
       extractedData.forEach(
         (blogId, blogData) {
-          RequestTaxi taxiRequest = RequestTaxi.fromJson(blogData);
-          print('INFO $blogData');
-          listRequestPreview.add(taxiRequest);
+          int idTaxi = blogData['idUserTaxi'];
+          if (idTaxi == idTaxista) {
+            idRequestAccept = blogId;
+            if (estado) {
+              statusRequest = true;
+            } else {
+              statusRequest = false;
+            }
+          }
         },
       );
-    statusRequest = false;
-
-    for (RequestTaxi item in listRequestPreview) {
-      String idTaxi = item.idTaxista;
-      print(idTaxi);
-      if (idTaxista == idTaxi) {
-        if (estado) {
-          statusRequest = true;
-        } else {
-          statusRequest = false;
-          print("No esta disponible pero tiene solicitudes");
-        }
-      }
     }
+
     updateStatus(statusRequest);
   }
 }
